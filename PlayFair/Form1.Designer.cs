4f9d8b8006bbe2778e4682e83966be463e96f862
hp <hp@DESKTOP-1K434U4>
2026-03-05 23:53:41 +0300

    Proje dosyası ekle.

---
+++ PlayFair/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace PlayFair
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.radioEncrypt = new System.Windows.Forms.RadioButton();
+            this.radioDecrypt = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(137, 22);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(207, 26);
+            this.txtKey.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Key:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Encrypted Text:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(507, 252);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(395, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 30);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(395, 22);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(246, 218);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Message:";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(137, 54);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(207, 109);
+            this.richTextBox2.TabIndex = 8;
+            this.richTextBox2.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "New Message:";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(137, 169);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(207, 109);
+            this.richTextBox3.TabIndex = 10;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(137, 284);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(207, 109);
+            this.richTextBox4.TabIndex = 11;
+            this.richTextBox4.Text = "";
+            // 
+            // radioEncrypt
+            // 
+            this.radioEncrypt.AutoSize = true;
+            this.radioEncrypt.Location = new System.Drawing.Point(522, 285);
+            this.radioEncrypt.Name = "radioEncrypt";
+            this.radioEncrypt.Size = new System.Drawing.Size(81, 24);
+            this.radioEncrypt.TabIndex = 12;
+            this.radioEncrypt.TabStop = true;
+            this.radioEncrypt.Text = "Encrypt";
+            this.radioEncrypt.UseVisualStyleBackColor = true;
+            // 
+            // radioDecrypt
+            // 
+            this.radioDecrypt.AutoSize = true;
+            this.radioDecrypt.Location = new System.Drawing.Point(522, 315);
+            this.radioDecrypt.Name = "radioDecrypt";
+            this.radioDecrypt.Size = new System.Drawing.Size(82, 24);
+            this.radioDecrypt.TabIndex = 13;
+            this.radioDecrypt.TabStop = true;
+            this.radioDecrypt.Text = "Decrypt";
+            this.radioDecrypt.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 409);
+            this.Controls.Add(this.radioDecrypt);
+            this.Controls.Add(this.radioEncrypt);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKey);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RadioButton radioEncrypt;
+        private System.Windows.Forms.RadioButton radioDecrypt;
+    }
+}
+
---
+++ PlayFair/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayFair
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public string key()
+        {
+            string key = txtKey.Text;
+            key = key.ToUpper().Replace("İ","I").Replace("J", "I").Replace(" ", "");
+            string alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < key.Length; i++)
+            {
+                char c = key[i];
+                if (!alphabet.Contains(c))
+                {
+                    return "Invalid Key";
+                }
+                if(!sb.ToString().Contains(c))
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+        public char[,] matrix(string key)
+        {
+            string alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
+            StringBuilder sb = new StringBuilder();
+            sb.Append(key);
+            char[,] matrix = new char[5, 5];
+            int index = 0;
+            foreach (char c in alphabet)
+            {
+                if (!sb.ToString().Contains(c))
+                {
+                    sb.Append(c);
+                }
+            }
+            string newalphabet = sb.ToString();
+            sb.Clear();
+            for (int row = 0; row <5; row++)
+            {
+                for(int col = 0; col < 5; col++)
+                {
+                    matrix[row, col] = newalphabet[index];
+                    index++;
+                }
+            }
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    sb.Append(matrix[row, col] + " ");
+                }
+                sb.AppendLine();
+            }
+
+            return matrix;
+        }
+        public string matrixtostring(char[,] matrix)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int r = 0; r < 5; r++)
+            {
+                for (int c = 0; c < 5; c++)
+                {
+                    sb.Append(matrix[r, c] + " ");
+                }
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 1) Key + matrix her iki modda da lazım
+            string cleanKey = key();
+            textBox1.Text = cleanKey;
+
+            // Key geçersizse devam etme (yoksa matris bozulur)
+            if (cleanKey == "Invalid Key" || string.IsNullOrEmpty(cleanKey))
+            {
+                MessageBox.Show("Key geçersiz. Sadece A-Z harf gir (J yok, J=I).");
+                return;
+            }
+
+            char[,] mat = matrix(cleanKey);
+            richTextBox1.Text = matrixtostring(mat);
+
+            if (radioEncrypt.Checked)
+            {
+                string prepared = PrepareMessage(richTextBox2.Text);
+                richTextBox3.Text = prepared;
+
+                string encrypted = EncryptMessage(prepared, mat);
+                richTextBox4.Text = encrypted;
+            }
+            else if (radioDecrypt.Checked)
+            {
+
+                string cipher = richTextBox4.Text;
+
+                cipher = cipher.ToUpper().Replace(" ", "");
+                if (cipher.Length % 2 == 1)
+                {
+                    MessageBox.Show("Şifreli metin uzunluğu tek olamaz. (Çift olmalı)");
+                    return;
+                }
+
+                string plain = DecryptMessage(cipher, mat);
+                plain = RemovePaddingX(plain);
+                richTextBox2.Text = plain;
+
+                richTextBox3.Clear();
+            }
+
+        }
+
+        public string PrepareMessage(string message)
+        {
+            message = message.ToUpper().Replace("İ", "I").Replace("J", "I").Replace(" ", "");
+            StringBuilder clean = new StringBuilder();
+            foreach(char c in message)
+            {
+                if(c >= 'A' && c <= 'Z')
+                {
+                    clean.Append(c);
+                }
+            }
+            StringBuilder result = new StringBuilder();
+            int i = 0;
+            while (i < clean.Length)
+            {
+                char first = clean[i];
+                char second = (i + 1 < clean.Length) ? clean[i + 1] : 'X';
+                if(first == second)
+                {
+                    result.Append(first);
+                    result.Append('X');
+                    i++;
+                }
+                if(first != second)
+                {
+                    result.Append(first);
+                    result.Append(second);
+                    i += 2;
+                }
+            }
+            return result.ToString();
+        }
+        public void FindPos(char[,] mat, char ch, out int row, out int col)
+        {
+            if (ch == 'J') ch = 'I';
+
+            for (int r = 0; r < 5; r++)
+            {
+                for (int c = 0; c < 5; c++)
+                {
+                    if (mat[r, c] == ch)
+                    {
+                        row = r;
+                        col = c;
+                        return;
+                    }
+                }
+            }
+
+            row = -1;
+            col = -1;
+        }
+        public string EncryptPair(char[,] mat, char a, char b)
+        {
+
+            if (a == 'J') a = 'I';
+            if (b == 'J') b = 'I';
+
+            int r1, c1, r2, c2;
+            FindPos(mat, a, out r1, out c1);
+            FindPos(mat, b, out r2, out c2);
+
+
+            if (r1 == r2)
+            {
+                c1 = (c1 + 1) % 5;
+                c2 = (c2 + 1) % 5;
+            }
+
+            else if (c1 == c2)
+            {
+                r1 = (r1 + 1) % 5;
+                r2 = (r2 + 1) % 5;
+            }
+
+            else
+            {
+                int temp = c1;
+                c1 = c2;
+                c2 = temp;
+            }
+
+            return $"{mat[r1, c1]}{mat[r2, c2]}";
+        }
+        public string EncryptMessage(string prepared, char[,] mat)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < prepared.Length; i += 2)
+            {
+                char a = prepared[i];
+                char b = prepared[i + 1];
+
+                sb.Append(EncryptPair(mat, a, b));
+            }
+
+            return sb.ToString();
+        }
+        public string DecryptPair(char[,] mat, char a, char b)
+        {
+            if (a == 'J') a = 'I';
+            if (b == 'J') b = 'I';
+
+            int r1, c1, r2, c2;
+            FindPos(mat, a, out r1, out c1);
+            FindPos(mat, b, out r2, out c2);
+
+            if (r1 == r2)
+            {
+                c1 = (c1 + 4) % 5;   
+                c2 = (c2 + 4) % 5;
+            }
+
+            else if (c1 == c2)
+            {
+                r1 = (r1 + 4) % 5;   
+                r2 = (r2 + 4) % 5;
+            }
+            else
+            {
+                int temp = c1;
+                c1 = c2;
+                c2 = temp;
+            }
+
+            return $"{mat[r1, c1]}{mat[r2, c2]}";
+        }
+        public string DecryptMessage(string cipher, char[,] mat)
+        {
+            cipher = cipher.ToUpper().Replace("İ", "I").Replace("J", "I").Replace(" ", "");
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < cipher.Length; i += 2)
+            {
+                char a = cipher[i];
+                char b = cipher[i + 1];
+                sb.Append(DecryptPair(mat, a, b));
+            }
+
+            return sb.ToString();
+        }
+        public string RemovePaddingX(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return text;
+
+            text = text.ToUpper().Replace("İ", "I");
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char current = text[i];
+
+                if (current == 'X' && i > 0 && i < text.Length - 1)
+                {
+                    char left = text[i - 1];
+                    char right = text[i + 1];
+
+                    if (left == right)
+                    {
+                        continue;
+                    }
+                }
+
+                sb.Append(current);
+            }
+
+            if (sb.Length > 0 && sb[sb.Length - 1] == 'X')
+            {
+                sb.Remove(sb.Length - 1, 1);
+            }
+
+            return sb.ToString();
+        }
+
+
+    }
+
+}
